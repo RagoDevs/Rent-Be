e0bba49f0bca2dbba927ae2257b296ef06c44b5d
Hopertz <lugano.ngulwa@gmail.com>
2023-03-25 06:49:54 +0300

    fix url

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,10 +15,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/v1/ping", app.ping)
 
-	// test 
+	// test
 
-	router.HandlerFunc(http.MethodGet,"/test", app.requireActivatedUser(app.test))
-
+	router.HandlerFunc(http.MethodGet, "/v1/test", app.requireActivatedUser(app.test))
 
 	// User Routes
 
@@ -26,19 +25,15 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-  
-
 	// Resend user token route
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
-
 
 	// Metrics Routes
 	router.Handler(http.MethodGet, "/v1/metrics", expvar.Handler())
 
-
 	//password management
 
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/passwordreset",app.createPasswordResetTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/passwordreset", app.createPasswordResetTokenHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
 
 	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
