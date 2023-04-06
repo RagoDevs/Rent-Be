f84183a006399b2fb5d4e33d50f75bbd7e9008fa
Hopertz <lugano.ngulwa@gmail.com>
2023-04-06 06:45:47 +0300

    Update migration

--- migrations/000004_create_houses_table.up.sql
+++ migrations/000004_create_houses_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS houses (
   house_id UUID PRIMARY KEY NOT NULL,
   location TEXT NOT NULL,
   block TEXT NOT NULL,
-  partition TEXT NOT NULL
+  partition TEXT NOT NULL,
+  occupied BOOL NOT NULL
 ); 
