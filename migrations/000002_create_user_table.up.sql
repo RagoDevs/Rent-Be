2a076922e36acff99b0704d4d57bd2c3459338a9
Hopertz <lugano.ngulwa@gmail.com>
2023-04-07 12:24:25 +0300

    Update admins sql migrations

--- migrations/000002_create_user_table.up.sql
+++ migrations/000002_create_user_table.up.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION citext;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS admins (
     admin_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     email citext UNIQUE NOT NULL,
