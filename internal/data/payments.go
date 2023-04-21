aaac6d4083cda5f841d9f043d019a43563ef7503
Hopertz <lugano.ngulwa@gmail.com>
2023-04-21 09:20:30 +0300

    Add ability to get unrenewed payments

--- internal/data/payments.go
+++ internal/data/payments.go
@@ -73,6 +73,85 @@ func (p PaymentModel) Get(payment_id string) (*Payment, error) {
 
 }
 
+
+func (p PaymentModel) GetUnrenewed(tenant_id string) (*Payment, error) {
+	query := `SELECT payment_id,tenant_id, period, start_date, end_date, renewed FROM payments 
+	WHERE renewed = false and tenant_id = $1 `
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	var payment Payment
+
+	err := p.DB.QueryRowContext(ctx, query, tenant_id).Scan(
+		&payment.PaymentId,
+		&payment.TenantId,
+		&payment.Period,
+		&payment.StartDate,
+		&payment.EndDate,
+		&payment.Renewed,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+
+		default:
+			return nil, err
+
+		}
+	}
+
+	return &payment, nil
+
+}
+
+func (p PaymentModel) GetAll() ([]*Payment, error) {
+	query := `SELECT payment_id,tenant_id, period, start_date, end_date, renewed FROM payments`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer cancel()
+
+	rows, err := p.DB.QueryContext(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	payments := []*Payment{}
+
+	for rows.Next() {
+		var payment Payment
+
+		err := rows.Scan(
+			&payment.PaymentId,
+			&payment.TenantId,
+			&payment.Period,
+			&payment.StartDate,
+			&payment.EndDate,
+			&payment.Renewed,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		payments = append(payments, &payment)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return payments, nil
+
+}
+
 func (p PaymentModel) Update(payment Payment) error {
 	query := `UPDATE payments
 	SET period = $1, start_date = $2, end_date = $3, renewed = $4
