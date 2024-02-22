e0df851d327cd4cdaa365352b3984e5437d99c07
Hopertz <lugano.ngulwa@gmail.com>
2024-02-22 05:46:49 +0300

    update routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -25,46 +25,42 @@ func (app *application) routes() http.Handler {
 
 	// User Routes
 
-	e.POST("/v1/users", app.registerUserHandler)
-	e.PUT("/v1/users/activated", app.activateUserHandler)
-	e.POST("/v1/tokens/authentication", app.createAuthenticationTokenHandler)
-	e.POST("/v1/tokens/activation", app.createActivationTokenHandler)
+	e.POST("/v1/admin", app.registerUserHandler)
+	e.PUT("/v1/admin/activate", app.activateUserHandler)
+	e.POST("/v1/login", app.createAuthenticationTokenHandler)
+	e.POST("/v1/token/activation", app.createActivationTokenHandler)
 
+
+	// password management
+	e.GET("/v1/token/passwordreset", app.createPasswordResetTokenHandler)
+	e.PUT("/v1/admin/password", app.updateUserPasswordHandler)
 
 	g := e.Group("/v1/auth")
 
 	g.Use(app.authenticate)
 
-	// User Routes
 
-	g.POST("/v1/users", app.registerUserHandler)
-	g.PUT("/v1/users/activated", app.activateUserHandler)
-	g.POST("/v1/tokens/authentication", app.createAuthenticationTokenHandler)
-	g.POST("/v1/tokens/activation", app.createActivationTokenHandler)
 
 	// houses
-	g.GET("/houses", app.listHousesHandler)
-	g.POST("/houses", app.createHouseHandler)
+	g.GET("/house", app.listHousesHandler)
+	g.POST("/house", app.createHouseHandler)
 	// g.POST("/bulk/houses", app.bulkHousesHandler)
-	g.GET("/houses/:uuid", app.showHousesHandler)
-	g.PUT("/houses/:uuid", app.updateHouseHandler)
+	g.GET("/house/:uuid", app.showHousesHandler)
+	g.PUT("/house/:uuid", app.updateHouseHandler)
 
 	// tenants
-	g.GET("/v1/tenants", app.listTenantsHandler)
-	g.POST("/v1/tenants", app.createTenantHandler)
-	g.GET("/v1/tenants/:uuid", app.showTenantsHandler)
-	g.PUT("/v1/tenants/:uuid", app.updateTenantsHandler)
-	g.DELETE("/v1/tenants/:uuid", app.removeTenant)
+	g.GET("/v1/tenant", app.listTenantsHandler)
+	g.POST("/v1/tenant", app.createTenantHandler)
+	g.GET("/v1/tenant/:uuid", app.showTenantsHandler)
+	g.PUT("/v1/tenant/:uuid", app.updateTenantsHandler)
+	g.DELETE("/v1/tenant/:uuid", app.removeTenant)
 
 	// Payments
-	g.GET("/v1/payments", app.listPaymentsHandler)
-	g.POST("/v1/payments", app.createPaymentHandler)
-	g.GET("/v1/payments/:uuid", app.showPaymentHandler)
-	g.PUT("/v1/payments/:uuid", app.updatPaymentHandler)
+	g.GET("/v1/payment", app.listPaymentsHandler)
+	g.POST("/v1/payment", app.createPaymentHandler)
+	g.GET("/v1/payment/:uuid", app.showPaymentHandler)
+	g.PUT("/v1/payment/:uuid", app.updatPaymentHandler)
 
-	//password management
-	g.GET("/v1/tokens/passwordreset", app.createPasswordResetTokenHandler)
-	g.PUT("/v1/users/password", app.updateUserPasswordHandler)
 
 	return e
 
