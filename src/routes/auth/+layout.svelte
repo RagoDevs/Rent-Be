47534d0c8ceeee5c809d9269a0dfe542aa7be624
Hopertz <lugano.ngulwa@gmail.com>
2024-03-19 17:24:33 +0300

    add dashboard link to layout

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -2,5 +2,8 @@
   <form action="/logout" method="POST">
     <button class="btn btn-primary">Logout</button>
   </form>
+
+ <a href="/auth/dashboard" class="btn btn-primary">Dashboard</a>
+ 
 </div>
 <slot />
