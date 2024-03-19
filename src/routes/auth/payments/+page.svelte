5c12ce35892e67b16efdd57a574678bc5b71d026
Hopertz <lugano.ngulwa@gmail.com>
2024-03-19 14:46:19 +0300

    update get payments

--- src/routes/auth/payments/+page.svelte
+++ src/routes/auth/payments/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  export let data: PageData;
+</script>
+
 <h1>Welcome to Payments</h1>
+
+{#each data.payments as payment}
+  <div>
+    <a href={`/auth/payments/${payment.id}`}>
+      <p>
+        Amount: {payment.amount}, Start Date: {payment.start_date}, End Date: {payment.end_date}
+      </p>
+    </a>
+  </div>
+{/each}
+
 
