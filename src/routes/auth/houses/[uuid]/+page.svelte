7c14d28ac5319300eabb1e73730a5be36da7399c
Hopertz <lugano.ngulwa@gmail.com>
2024-03-19 13:58:13 +0300

    add house page

---
+++ src/routes/auth/houses/[uuid]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  export let data: PageData;
+</script>
+
+<h1>House</h1>
+
+<div>
+  <p>
+    Location : <strong>{data.house.location}</strong>, Block {data.house.block},
+    Partition {data.house.partition}, Occuppied {data.house.occupied}
+  </p>
+</div>
