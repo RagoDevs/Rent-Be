f238dd8d8f001824a6a69e716164885fa44eb7ad
Hopertz <lugano.ngulwa@gmail.com>
2024-03-21 08:32:29 +0300

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}</h1>
+<h3> we are going to edit this page</h3>
+<!-- <p>{$page.error.message}</p>
+<p>error code: {$page.error.code}</p> -->
